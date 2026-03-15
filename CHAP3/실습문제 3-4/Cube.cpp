8b7cbd71b12fe696393224cf48412879ff0e9999
peace9223 <jhw0404052@gmail.com>
2026-03-15 23:35:38 +0900

    실문 3-4

---
+++ CHAP3/실습문제 3-4/Cube.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "Cube.h";
+using namespace std;
+
+Cube::Cube(int a, int b, int c)
+{
+	width = a;
+	height = b;
+	length = c;
+}
+
+int Cube::getVolume()
+{
+	return width * height * length;
+}
+
+void Cube::increase(int a, int b, int c)
+{
+	width += a;
+	height += b;
+	length += c;
+}
+
+bool Cube::isZero()
+{
+	if (getVolume() == 0)
+		return true;
+	else
+		return false;
+}
---
+++ CHAP3/실습문제 3-4/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "Cube.h"
+using namespace std;
+
+int main()
+{
+	Cube cube(1, 2, 3);
+	cout << "큐브
+		
+		의 부피는 " << cube.getVolume() << endl;
+	cube.increase(1, 2, 3);
+	cout << "큐브의 부피는 " << cube.getVolume() << endl;
+	if (cube.isZero()) cout << "큐브의 부피는 0" << endl;
+	else cout << "큐브의 부피는 0이 아님" << endl;
+}
