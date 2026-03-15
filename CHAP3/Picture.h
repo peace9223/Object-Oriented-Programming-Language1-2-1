f1e9ab98d12359470323c9f69efb43459f2b54e2
peace9223 <jhw0404052@gmail.com>
2026-03-15 23:18:02 +0900

    실습문제 3-1

---
+++ CHAP3/Picture.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+/*
+ifndef
+define
+endif
+*/
+#include <string>
+using namespace std;
+class Picture
+{
+private:
+	int width;
+	int height;
+	string place;
+public:
+	Picture();
+	Picture(int a, int b, string str);
+	int getWidth();
+	int getHeight();
+	string getPlace();
+};
