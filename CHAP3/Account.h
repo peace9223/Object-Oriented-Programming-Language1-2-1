fe17565e02fe142ee33a4cff3bebaea8dbe301ce
peace9223 <jhw0404052@gmail.com>
2026-03-15 23:22:47 +0900

    실습문제 3-3

---
+++ CHAP3/Account.h
@@ -0,0 +1,22 @@
+﻿
+#pragma once
+#include <string>
+using namespace std;
+/*
+#ifndef Acount
+#define Acount
+#endif
+*/
+
+class Account
+{
+private:
+	string name;
+	int money;
+public:
+	Account(string s);
+	void deposit(int a); // 저금
+	string getOwner(); // 은행 계좌 소유주 이름
+	int inqury(); // 현재 잔액 출력
+	int withdraw(int a); // 출금
+};
