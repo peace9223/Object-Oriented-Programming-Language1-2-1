dd92ce4f411979e76cb51dbb2d82e93b1aa859ed
peace9223 <jhw0404052@gmail.com>
2026-03-15 23:23:35 +0900

    실문 3-3

---
+++ CHAP3/실습문제 3-3/Account.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include "Account.h"
+using namespace std;
+
+Account::Account(string s)
+{
+	name = s;
+	money = 0;
+}
+
+void Account::deposit(int a)
+{
+	money += a;
+}
+
+string Account::getOwner()
+{
+	return name;
+}
+
+int Account::inqury()
+{
+	return money;
+}
+
+int Account::withdraw(int a)
+{
+	if (money < a)
+	{
+		cout << "잔액이 부족합니다 " <<a<<"원만 출금합니다!" <<endl;
+		money = 0;
+		return a;
+	}
+	else
+	{
+		money -= a;
+		return a;
+	}
+}
---
+++ CHAP3/실습문제 3-3/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Account.h"
+using namespace std;
+
+int main()
+{
+	Account a("황수희");
+	a.deposit(20000);
+	cout << a.getOwner() << " 잔액은 " << a.inqury() << endl;
+	int money = a.withdraw(15000);
+	cout << money << "원 출금, ";
+	cout << a.getOwner() << " 잔액은 " << a.inqury() << endl;
+	money = a.withdraw(8000);
+	cout << money << "원 출금, ";
+	cout << a.getOwner() << " 잔액은 " << a.inqury() << endl;
+}
