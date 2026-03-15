a92344a90a0c22684ec849c2cdf1c8c5975a8351
peace9223 <jhw0404052@gmail.com>
2026-03-15 23:34:56 +0900

    실문 3-4

---
+++ CHAP3/Cube.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+/*
+#ifndef Cube
+#define Cube
+#endif
+*/
+
+class Cube
+{
+private:
+	int width; // 가로
+	int height; // 세로
+	int length; // 높이
+public:
+	Cube(int a, int b, int c);
+	int getVolume(); //  부피 반환
+	void increase(int a, int b, int c); // 부피 증가
+	bool isZero(); // 부피가 0인지 아닌지
+};
+
+
