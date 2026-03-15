3d6bb8545f438b400df690856a5d464118609548
peace9223 <jhw0404052@gmail.com>
2026-03-15 23:18:36 +0900

    실습문제 3-1

---
+++ CHAP3/Picture.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+#include "Picture.h"
+
+Picture::Picture()
+{
+	width = 5;
+	height = 7;
+	place = "장소 모름";
+}
+
+Picture::Picture(int a, int b, string str)
+{
+	width = a;
+	height = b;
+	place = str;
+}
+
+int Picture::getWidth()
+{
+	return width;
+}
+
+int Picture::getHeight()
+{
+	return height;
+}
+
+string Picture::getPlace()
+{
+	return place;
+}
---
+++ CHAP3/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "Picture.h"
+using namespace std;
+
+int main()
+{
+	Picture pic;
+	Picture mt(10, 14, "한라산");
+	cout << pic.getWidth() << "x" << pic.getHeight() << " " << pic.getPlace() << endl;
+	cout << mt.getWidth() << "x" << mt.getHeight() << " " << mt.getPlace() << endl;
+}
