edbf8897c5fc16fcd4a7c681fd5d93d70eb1dedb
peace9223 <jhw0404052@gmail.com>
2026-03-15 23:16:11 +0900

    지수만들기지만 예시파일

---
+++ Exp.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+/*
+ifndef
+define
+
+endif
+*/
+
+class Exp
+{
+private:
+	int base;
+	int exp;
+public:
+	Exp();
+	Exp(int a);
+	Exp(int a, int b);
+
+	int getValue(); // 제곱의 결과값
+	int getBase(); // int base
+	int getExp(); // int exp;
+	bool equal(Exp b);
+};
